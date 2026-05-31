75c4ba73ec1cd7ec20d756215093f0bc48f81963
pravallika-v <pravallikavuyyuru10@gmail.com>
2026-05-31 20:34:59 +0530

    Add files via upload
    Added number search project using loops and break

---
+++ number search.py
@@ -0,0 +1,14 @@
+numbers = [10,20,30,40,50]
+target = 30
+found = False
+for i in numbers:
+    if i == target:
+        found = True
+        break
+if found:
+    print("Number found")
+else:
+    print("Number not found")
+
+        
+
